1594ebbee9e49d1e0255fef8cf2da71b5c5e43c7
Maclemon17 <maclemon17@gmail.com>
2025-08-25 15:16:55 +0100

    fix: adjust Hero component text size and layout for better responsiveness
    - update gitignore

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
 
             <div className="flex flex-col-reverse md:flex-row justify-between items-center gap-10">
                 <div className="hero-text-box flex-1 text-center md:text-left">
-                    <h1 className="text-5xl md:text-8xl/tight text-dark-blue font-extrabold">More than just <br /> shorter links</h1>
+                    <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-[60px] text-dark-blue font-extrabold">More than just <br /> shorter links</h1>
 
                     <p className="text-gray font-medium md:text-2xl max-w-xl mt-4 md:mt-2 mb-8">
                         Build your brand’s recognition and get detailed insights
@@ -16,7 +16,7 @@ const Hero = () => {
                     <button className='rounded-full py-4 px-15 text-white font-bold bg-cyan hover:opacity-70 transition cursor-pointer'>Get Started</button>
                 </div>
 
-                <div className="hero-img-box">
+                <div className="hero-img-box flex-1">
                     <img src={heroImage} alt="illustration-working" className="-right-30" />
 
                 </div>
